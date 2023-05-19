1e605a49f3c02999a51475c7cc9673c2e689f4df
manueletz <manueletz@hotmail.com>
2023-05-19 07:20:48 -0600

    update

---
+++ src/main/java/com/alura/pruebas/PruebaHuesped.java
@@ -0,0 +1,32 @@
+package com.alura.pruebas;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.alura.modelo.Huesped;
+import com.mysql.cj.protocol.x.SyncFlushDeflaterOutputStream;
+
+public class PruebaHuesped {
+	public static void main(String[] args) {
+		Date date = new Date(); 
+		SimpleDateFormat dateformat = new SimpleDateFormat("dd-MM-yyyy");
+		
+		//Date date = new Date();
+		
+		try {
+		date = dateformat.parse("18-05-2023");
+		System.out.println(dateformat.format(date));
+		}catch(ParseException e) {
+			throw new RuntimeException(e);
+		}
+		
+		
+		Huesped huesped = new Huesped(1, "manuel", "estevez", '2023-05-05', "Salvadoreña", "77887788");
+		
+		System.out.println(huesped);
+		
+		
+	}
+
+}
